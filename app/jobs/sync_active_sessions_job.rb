c821c77947be56416550ca4ded6c569c8233bf5b
Abhi Devireddy <abhi@devireddy.com>
2019-11-04 10:21:20 -0600

    Fixed invalid MAC addr error.

--- app/jobs/sync_active_sessions_job.rb
+++ app/jobs/sync_active_sessions_job.rb
@@ -19,14 +19,18 @@ class SyncActiveSessionsJob < ApplicationJob
     ActiveRecord::Base.connection.execute("UPDATE sessions SET active = false")
     # Recreate new sessions
     sessions.each do |session|
-      ar_session = Session.find_or_create_by(mac: session["calling_station_id"])
-      ar_session.active = true
-      ar_session.mac = session["calling_station_id"]
-      ar_session.username = session["user_name"]
-      ar_session.ip_address = session["framed_ip_address"]
-      ar_session.audit_session_id = session["audit_session_id"]
+      begin
+        ar_session = Session.find_or_create_by(mac: session["calling_station_id"])
+        ar_session.active = true
+        ar_session.mac = session["calling_station_id"]
+        ar_session.username = session["user_name"]
+        ar_session.ip_address = session["framed_ip_address"]
+        ar_session.audit_session_id = session["audit_session_id"]
 
-      ar_session.save
+        ar_session.save
+      rescue
+        logger.error("Invalid Session: #{session}")
+      end
     end
   end
 end
